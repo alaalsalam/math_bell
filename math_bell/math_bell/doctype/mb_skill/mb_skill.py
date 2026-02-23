2367e935052e036c923d95165d7eaa295d04bd33
alaalsalam <alaalsalam101@gmail.com>
2026-02-23 22:54:45 +0100

    feat: add prerequisites and pack fields to skills

--- math_bell/math_bell/doctype/mb_skill/mb_skill.py
+++ math_bell/math_bell/doctype/mb_skill/mb_skill.py
@@ -1,16 +1,14 @@
-# Copyright (c) 2026, alaalsalam and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
 import frappe
+from frappe.model.document import Document
 
 from math_bell.utils.validators import ensure_link_exists
+from math_bell.api.helpers import parse_doc_json
 
 
 class MBSkill(Document):
     def validate(self):
-        ensure_link_exists('MB Grade', self.grade, 'Grade')
-        ensure_link_exists('MB Domain', self.domain, 'Domain')
+        ensure_link_exists("MB Grade", self.grade, "Grade")
+        ensure_link_exists("MB Domain", self.domain, "Domain")
 
         if not self.generator_type:
             self.generator_type = "static"
@@ -21,3 +19,13 @@ class MBSkill(Document):
             self.difficulty_min = 1
         if self.difficulty_max < self.difficulty_min:
             frappe.throw("difficulty_max must be greater than or equal to difficulty_min")
+
+        if not self.unlock_rule:
+            self.unlock_rule = "by_mastery"
+        if self.unlock_rule not in {"by_mastery", "manual"}:
+            frappe.throw("unlock_rule must be either 'by_mastery' or 'manual'")
+
+        if self.prerequisites_json:
+            parsed = parse_doc_json(self.prerequisites_json)
+            if not isinstance(parsed, list):
+                frappe.throw("prerequisites_json must be a JSON array of skill codes")
