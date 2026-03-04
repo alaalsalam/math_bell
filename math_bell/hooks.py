a2dace837e63d07152ae9d11c03f14907e6c24c7
alaalsalam <alaalsalam101@gmail.com>
2026-03-04 23:14:50 +0100

    chore(web): set frontend page as default home route

--- math_bell/hooks.py
+++ math_bell/hooks.py
@@ -57,7 +57,7 @@ app_license = "mit"
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = "math-bell-games"
 
 # website user home page (by Role)
 # role_home_page = {
@@ -246,4 +246,3 @@ app_license = "mit"
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
-
